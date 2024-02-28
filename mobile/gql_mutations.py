b66f477910fd3cc62c5685f7fd4a0c76004d66e2
Thibault Dethier <tdethier@bluesquarehub.com>
2024-02-28 18:51:44 +0100

    Added PolicyRenewalAndPremium mutation
    Added PolicyRenewalAndPremium mutation for allowing the mobile app to renew a Policy and pay for the new policy in a single endpoint.
    This mutation is based on an existing PolicyRenewal object.

--- mobile/gql_mutations.py
+++ mobile/gql_mutations.py
@@ -11,11 +11,13 @@ from contribution.gql_mutations import PremiumBase, update_or_create_premium
 from core.schema import OpenIMISMutation
 from insuree.gql_mutations import FamilyBase, InsureeBase
 from insuree.services import FamilyService, InsureeService
-from policy.gql_mutations import PolicyInputType
-from policy.models import Policy
-from policy.services import PolicyService
+from payer.models import Payer
+from policy.gql_mutations import PolicyInputType, CreateRenewOrUpdatePolicyMutation
+from policy.gql_queries import PolicyGQLType
+from policy.models import Policy, PolicyRenewal
+from policy.services import PolicyService, process_create_renew_or_update_policy
 from mobile.apps import MobileConfig
-
+from policy.values import policy_values
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +43,6 @@ class MobileEnrollmentGQLType:
     insurees = graphene.List(InsureeEnrollmentGQLType)  # for families with more than the head insuree
     policies = graphene.List(PolicyEnrollmentGQLType, required=True)
     premiums = graphene.List(PremiumEnrollmentGQLType, required=True)
-
 
 
 MOBILE_ENROLLMENT_RIGHTS = [
@@ -151,5 +152,126 @@ def delete_none(_dict):
     return _dict
 
 
+class MobilePolicyRenewalAndPremiumGQLType:
+    renewal_id = graphene.Int(required=True)
+    renewal_date = graphene.Date(required=True)
+    officer_id = graphene.Int(required=True)
+    receipt = graphene.String(required=True)
+    pay_type = graphene.String(required=True, max_length=1)
+    amount = graphene.Decimal(max_digits=18, decimal_places=2, required=True)
+    payer_id = graphene.Int(required=False)
+
+    # The fields under this comment are used when creating a new renewal from scratch from the mobile app
+    # renewal_id = graphene.Int(required=False)
+    # chf_id = graphene.String(required=False)
+    # product_code = graphene.String(required=False)
+    # product_id = graphene.String(required=False)
+
+
+MOBILE_POLICY_RENEWAL_AND_PREMIUM_RIGHTS = [
+    MobileConfig.gql_mutation_renew_policies_perms,
+    MobileConfig.gql_mutation_create_premiums_perms,
+]
+
+
+class MobilePolicyRenewalAndPremium(CreateRenewOrUpdatePolicyMutation):
+    _mutation_module = "mobile"
+    _mutation_class = "MobilePolicyRenewalAndPremiumMutation"
+
+    class Input(MobilePolicyRenewalAndPremiumGQLType, OpenIMISMutation.Input):
+        pass
+
+    @classmethod
+    def async_mutate(cls, user, **data):
+        logger.info("Receiving new mobile policy renewal & premium request")
+        logger.info(data)
+        try:
+            with transaction.atomic():
+                if type(user) is AnonymousUser or not user.id:
+                    raise ValidationError("mutation.authentication_required")
+                if any(not user.has_perms(right) for right in MOBILE_POLICY_RENEWAL_AND_PREMIUM_RIGHTS):
+                    raise PermissionDenied("unauthorized")
+
+                renewal_id = data["renewal_id"]
+                policy_renewal = PolicyRenewal.objects.filter(validity_to__isnull=True, id=renewal_id).first()
+                if not policy_renewal:
+                    error_message = f"Error - unknown PolicyRenewal - ID={renewal_id}"
+                    logger.error(error_message)
+                    raise ValueError(error_message)
+
+                logger.info(f"Processing policy renewal ID {renewal_id}")
+                old_policy = policy_renewal.policy
+                product = policy_renewal.new_product
+                family = policy_renewal.insuree.family
+                new_policy_start_date = policy_renewal.renewal_date
+                renewal_received_amount = data["amount"]
+
+                # 1st step is to reproduce the calls the FE web app does to fetch all the necessary data for creating the new policy
+                policy_preparation_data = PolicyGQLType(
+                    stage=Policy.STAGE_RENEWED,
+                    enroll_date=new_policy_start_date,
+                    start_date=new_policy_start_date,
+                    product=product,
+                )
+                missing_policy_data, warnings = policy_values(policy_preparation_data, family, old_policy)
+
+                # Doing some checks to make sure that the policy can be created
+                if warnings and len(warnings):
+                    logger.error("There were some warnings with the preparation of the new policy")
+                    return warnings
+                renewed_policy_value = missing_policy_data.value
+                if renewal_received_amount < renewed_policy_value:
+                    error_message = (f"Error - payment is too low to renew policy - "
+                                     f"required amount={renewed_policy_value}, "
+                                     f"received amount={renewal_received_amount}")
+                    logger.error(error_message)
+                    raise ValueError(error_message)
+                elif renewal_received_amount == renewed_policy_value:
+                    logger.info("The required amount matches the received amount")
+                else:
+                    logger.info("The received amount is higher than the required amount")
+
+                renewed_policy_data = {
+                    "status": Policy.STATUS_IDLE,
+                    "stage": Policy.STAGE_RENEWED,
+                    "enroll_date": data["renewal_date"],
+                    "start_date": missing_policy_data.start_date,
+                    "expiry_date": missing_policy_data.expiry_date,
+                    "value": renewed_policy_value,
+                    "product_id": product.id,
+                    "family_id": family.id,
+                    "officer_id": user.officer_id,
+                }
+                renewed_policy, errors = process_create_renew_or_update_policy(user, renewed_policy_data)
+                if errors and len(errors):
+                    logger.error("There were some error with the new policy")
+                    return errors
+
+                # Now that the new policy was created, the premium can be created too
+                payer = None
+                if "payer_id" in data:
+                    payer = Payer.objects.filter(id=data["payer_id"]).first()
+
+                premium_data = {
+                    "policy_uuid": renewed_policy.uuid,
+                    "amount": renewal_received_amount,
+                    "payer_uuid": payer.uuid if payer else None,
+                    "receipt": data["receipt"],
+                    "pay_date": data["renewal_date"],
+                    "pay_type": data["pay_type"],
+                    "is_photo_fee": False,
+                }
+                update_or_create_premium(premium_data, user)
+
+                return None
+        except Exception as exc:
+            return [
+                {
+                    'message': "core.mutation.failed_to_enroll",
+                    'detail': str(exc)
+                }]
+
+
 class Mutation(graphene.ObjectType):
     mobile_enrollment = MobileEnrollmentMutation.Field()
+    mobile_policy_renewal_and_premium = MobilePolicyRenewalAndPremium.Field()
